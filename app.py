667dda485d12bc350dbee52764c95e0f76bc746a
shili <shili@duole.com>
2024-12-06 17:23:32 +0800

    version-mysql-query

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import redis
 #用户DB的配置
 USERDB_HOST = "db"
 USERDB_PORT = 3306
-USERDB_USER = "root"
-USERDB_PASS = ""
-USERDB_DATABASE = "mysql"
+USERDB_USER = "duoletest"
+USERDB_PASS = "123456"
+USERDB_DATABASE = "duole"
 
 udb = web.database(
     dbn = "mysql",
@@ -41,9 +41,12 @@ class test:
             ursc.incr("testincr", 1)
             t = ursc.get("testincr")
             print 'test, t', t
-            user = udb.query("select * from user")
-            for r in user:
-                print r
+            user = udb.query("select * from purchaseblacklist where content = '70622'")
+            if not user:
+                udb.query("insert into purchaseblacklist(id, type, content, gameid, createtime) values(NULL, 5, '70622', 3, NOW())")
+            else:
+                r = user[0]
+                print 'user', r
         except Exception as e:
             print(e)
             return "query exception"
